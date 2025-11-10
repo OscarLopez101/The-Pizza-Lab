f65c11e82a65c8af03f3953f0f095a310d738da6
Student <oscar.lopez102004.ol@gmail.com>
2025-11-10 15:26:39 -0800

    added a toppings java class

--- src/main/Java/com/pluralsight/Topping.java
+++ src/main/Java/com/pluralsight/Topping.java
@@ -29,3 +29,79 @@ public class Topping {
     }
 
 
+     // Returns the price of this topping based on pizza size.
+      //Regular toppings and sauces are included (0 cost).
+
+    public double getPrice(PizzaSize size) {
+        switch (type) {
+            case MEAT:
+                return extra ? getExtraMeatPrice(size) : getMeatPrice(size);
+            case CHEESE:
+                return extra ? getExtraCheesePrice(size) : getCheesePrice(size);
+            case REGULAR:
+            case SAUCE:
+            default:
+                return 0.0; // Included
+        }
+    }
+
+    private double getMeatPrice(PizzaSize size) {
+        switch (size) {
+            case PERSONAL_8:
+                return 1.00;
+            case MEDIUM_12:
+                return 2.00;
+            case LARGE_16:
+                return 3.00;
+            default:
+                return 0.0;
+        }
+    }
+
+    private double getExtraMeatPrice(PizzaSize size) {
+        switch (size) {
+            case PERSONAL_8:
+                return 0.50;
+            case MEDIUM_12:
+                return 1.00;
+            case LARGE_16:
+                return 1.50;
+            default:
+                return 0.0;
+        }
+    }
+
+    private double getCheesePrice(PizzaSize size) {
+        switch (size) {
+            case PERSONAL_8:
+                return 0.75;
+            case MEDIUM_12:
+                return 1.50;
+            case LARGE_16:
+                return 2.25;
+            default:
+                return 0.0;
+        }
+    }
+
+    private double getExtraCheesePrice(PizzaSize size) {
+        switch (size) {
+            case PERSONAL_8:
+                return 0.30;
+            case MEDIUM_12:
+                return 0.60;
+            case LARGE_16:
+                return 0.90;
+            default:
+                return 0.0;
+        }
+    }
+
+    @Override
+    public String toString() {
+        String extraLabel = extra ? " (extra)" : "";
+        return name + extraLabel;
+    }
+}
+
+
