d5bf6ca6d281078437086773cc9f9c6bc75a06a5
Student <oscar.lopez102004.ol@gmail.com>
2025-11-12 13:54:22 -0800

    Added a menu if you click start a new order. Asking if you want to add a pizza, drink, garlic knots, or to canel the order

--- src/main/Java/com/pluralsight/Menu.java
+++ src/main/Java/com/pluralsight/Menu.java
@@ -31,6 +31,44 @@ public class Menu {
         }
     }
 
+    private void startNewOrder() {
+        Order order = new Order();
+        boolean ordering = true;
+
+        while (ordering) {
+            System.out.println("\n==== ORDER SCREEN ====");
+            System.out.println("1) Add Pizza");
+            System.out.println("2) Add Drink");
+            System.out.println("3) Add Garlic Knots");
+            System.out.println("4) Checkout");
+            System.out.println("0) Cancel Order");
+            System.out.print("Select option: ");
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    addPizza(order);
+                    break;
+                case "2":
+                    addDrink(order);
+                    break;
+                case "3":
+                    addGarlicKnots(order);
+                    break;
+                case "4":
+                    checkout(order);
+                    ordering = false;
+                    break;
+                case "0":
+                    System.out.println("Order cancelled.");
+                    ordering = false;
+                    break;
+                default:
+                    System.out.println("Invalid option.");
+            }
+        }
+    }
+
 
 
 
