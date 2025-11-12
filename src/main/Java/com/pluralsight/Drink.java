5fbff385df0be75d5286801f77d704205a8a612a
Student <oscar.lopez102004.ol@gmail.com>
2025-11-12 10:40:55 -0800

    updated the Drink.java class

--- src/main/Java/com/pluralsight/Drink.java
+++ src/main/Java/com/pluralsight/Drink.java
@@ -5,4 +5,39 @@ public class Drink implements Pricable {
     private DrinkSize size;
     private String flavor;
 
+    public Drink(DrinkSize size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    public DrinkSize getSize() {
+        return size;
+    }
+
+    public void setSize(DrinkSize size) {
+        this.size = size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    @Override
+    public double getPrice() {
+        return size.getPrice();
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s Drink (%s) - $%.2f",
+                size.name(),
+                flavor,
+                getPrice());
+    }
+}
+
 
