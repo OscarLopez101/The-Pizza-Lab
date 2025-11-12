c0e0ff4441161592bc9467b4ae778b34a36c8af7
Student <oscar.lopez102004.ol@gmail.com>
2025-11-12 10:56:00 -0800

    Added the GarlicKnots.java class

---
+++ src/main/Java/com/pluralsight/GarlicKnots.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class GarlicKnots implements Pricable {
+
+    public static final double PRICE_PER_ORDER = 1.50;
+
+    private int quantity; // number of orders of garlic knots
+
