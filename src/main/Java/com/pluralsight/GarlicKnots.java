5b1c86db0b4ba7c7e0e8ce70b10aacf47b048bd9
Student <oscar.lopez102004.ol@gmail.com>
2025-11-12 11:09:50 -0800

    Updated the GarlicKnots.java class

--- src/main/Java/com/pluralsight/GarlicKnots.java
+++ src/main/Java/com/pluralsight/GarlicKnots.java
@@ -6,3 +6,25 @@ public class GarlicKnots implements Pricable {
 
     private int quantity; // number of orders of garlic knots
 
+    public GarlicKnots(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void addQuantity(int amount) {
+        this.quantity += amount;
+    }
+
+    @Override
+    public double getPrice() {
+        return quantity * PRICE_PER_ORDER;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Garlic Knots x%d - $%.2f", quantity, getPrice());
+    }
+}
