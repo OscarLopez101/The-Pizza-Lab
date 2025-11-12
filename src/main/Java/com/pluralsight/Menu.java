37a3cfdaa959349613ffe3c3ba302876d5dd7a64
Student <oscar.lopez102004.ol@gmail.com>
2025-11-12 13:26:52 -0800

    Added the first menu screen asking if you want to start a new order.

--- src/main/Java/com/pluralsight/Menu.java
+++ src/main/Java/com/pluralsight/Menu.java
@@ -7,4 +7,30 @@ public class Menu {
     private Scanner scanner = new Scanner(System.in);
     private boolean running = true;
 
+    public void display() {
+        System.out.println("Welcome to PIZZA-licious!");
+        while (running) {
+            System.out.println("\n==== HOME SCREEN ====");
+            System.out.println("1) New Order");
+            System.out.println("0) Exit");
+            System.out.print("Select option: ");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    startNewOrder();
+                    break;
+                case "0":
+                    running = false;
+                    System.out.println("Goodbye!");
+                    break;
+                default:
+                    System.out.println("Invalid option, try again.");
+            }
+        }
+    }
+
+
+
 
