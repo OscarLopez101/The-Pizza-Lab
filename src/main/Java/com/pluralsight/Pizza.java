b5fb0e38c1769b9b2148a447d729e48f1f786d2c
Student <oscar.lopez102004.ol@gmail.com>
2025-11-12 10:15:41 -0800

    added a the Pizza.java class

--- src/main/Java/com/pluralsight/Pizza.java
+++ src/main/Java/com/pluralsight/Pizza.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
 
-public class Pizza {
-}
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza implements Pricable {
+
+    private PizzaSize size;
+    private CrustType crustType;
+    private boolean stuffedCrust;
+    private List<Topping> toppings = new ArrayList<>();
+
