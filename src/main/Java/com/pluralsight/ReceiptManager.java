e170f3f92e4464a42fa4d68c43823d9d51590b60
Student <oscar.lopez102004.ol@gmail.com>
2025-11-12 15:59:32 -0800

    finished the ReceiptManager.java class

--- src/main/Java/com/pluralsight/ReceiptManager.java
+++ src/main/Java/com/pluralsight/ReceiptManager.java
@@ -1,6 +1,10 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
 
 public class ReceiptManager {
 
@@ -8,4 +12,18 @@ public class ReceiptManager {
         try {
             File dir = new File("receipts");
             if (!dir.exists()) dir.mkdirs();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+            String filename = "receipts/" + order.getOrderTime().format(formatter) + ".txt";
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+                writer.write(order.toString());
+            }
+            System.out.println("Receipt saved: " + filename);
+        } catch (IOException e) {
+            System.err.println("Error saving receipt: " + e.getMessage());
+        }
+    }
+}
+
+
 
